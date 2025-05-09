3304aae95b8a8560f2d14ae9a1f3f2c9b1df3a75
Joe Ellis <joe@plaidhatgames.com>
2025-05-09 14:46:56 -0400

    Vite, react, tailwind, and sass.

---
+++ src/CardPrototyper.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CardPrototyper.scss';
+
+export default class CardPrototyper extends React.Component {
+  render() {
+    return (
+      <div className="card-prototyper">
+        <h1>Card Prototyper</h1>
+      </div>
+    );
+  }
+}
